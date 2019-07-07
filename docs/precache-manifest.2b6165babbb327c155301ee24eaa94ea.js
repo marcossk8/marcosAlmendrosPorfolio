3d946bd84e684c86ee990cba1eb60d0c781ae322
marcos almendros <marcosdalmendros@gmail.com>
2019-07-07 11:47:35 -0300

    folder docs

---
+++ docs/precache-manifest.2b6165babbb327c155301ee24eaa94ea.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2035d62f907f6e0cabd2a43c4ee774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc49f854c444cbee9478",
+    "url": "/static/css/main.5807d0c4.chunk.css"
+  },
+  {
+    "revision": "2f4149dd136f7527b1ad",
+    "url": "/static/js/2.fe8a4794.chunk.js"
+  },
+  {
+    "revision": "bc49f854c444cbee9478",
+    "url": "/static/js/main.f6d0d0a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7345c594d5e9430d150757842c169639",
+    "url": "/static/media/audall.7345c594.png"
+  },
+  {
+    "revision": "e2e8d1937228fb72b4d98f8b03b8dc86",
+    "url": "/static/media/bannerMA.e2e8d193.png"
+  },
+  {
+    "revision": "91d3f0ac57d369474d9bb8bf67ff5dd6",
+    "url": "/static/media/ccu.91d3f0ac.png"
+  },
+  {
+    "revision": "3d42a2739b8c4344b8f1955a0eaf90d3",
+    "url": "/static/media/conquer.3d42a273.png"
+  },
+  {
+    "revision": "fa4e7dbe8024ff05712a1ea2af3fbe88",
+    "url": "/static/media/dasdasur.fa4e7dbe.png"
+  },
+  {
+    "revision": "52e9b71fd098753fc7710f49da6e8ffe",
+    "url": "/static/media/duty.52e9b71f.png"
+  },
+  {
+    "revision": "f5805195e5bca7a05b0e823b1583f7f6",
+    "url": "/static/media/dutyauditor.f5805195.png"
+  },
+  {
+    "revision": "d1ff36766f406cf979e8b1d349776597",
+    "url": "/static/media/fidelizacion.d1ff3676.png"
+  },
+  {
+    "revision": "9d038ab2a2443275729c27201c0d5593",
+    "url": "/static/media/logoMA.9d038ab2.svg"
+  },
+  {
+    "revision": "cac6b2a70af7e37a2a3a4e77887f8b0d",
+    "url": "/static/media/logoangular.cac6b2a7.svg"
+  },
+  {
+    "revision": "3287e9de55ec25eb1b15f4b9c478b448",
+    "url": "/static/media/logocss.3287e9de.svg"
+  },
+  {
+    "revision": "a44e6c8195602689a024c1767cbc3147",
+    "url": "/static/media/logohtml.a44e6c81.svg"
+  },
+  {
+    "revision": "087bc7cadc2d15a8636c74b24cbec28f",
+    "url": "/static/media/logoillustrator.087bc7ca.svg"
+  },
+  {
+    "revision": "2981d40ac099e5372399a39a2ba887d5",
+    "url": "/static/media/logoindesing.2981d40a.svg"
+  },
+  {
+    "revision": "2f53053166595d227b2252fe54987973",
+    "url": "/static/media/logojavascript.2f530531.svg"
+  },
+  {
+    "revision": "a1672c840a271effebd1799fe875d71a",
+    "url": "/static/media/logophotoshop.a1672c84.svg"
+  },
+  {
+    "revision": "22c5c6e80e836490c55e24d4e8022edc",
+    "url": "/static/media/logoreact.22c5c6e8.svg"
+  },
+  {
+    "revision": "590dbbc0f82e07c7cf0cd20a041d8bf7",
+    "url": "/static/media/panelduty.590dbbc0.png"
+  },
+  {
+    "revision": "8d08257b79120efbd304f12f753ef7db",
+    "url": "/static/media/saco.8d08257b.png"
+  }
+]);
